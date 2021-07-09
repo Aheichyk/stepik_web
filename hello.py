b08eef21492c243e7a961b33fa2c58596cbf40f3
root <yury.ahechyk@gmail.com>
2021-07-09 07:33:39 -0700

    wcgi,2

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 def process_http_request(environ, start_response):
-    param = environ.get("QUERY_STRING")
     status = '200 OK'
     response_headers = [
         ('Content-type', 'text/plain'),
     ]
     start_response(status, response_headers)
-    return [param.replace('&','\n')]
+    body = [(i + '\n') for i in environ['QUERY_STRING'].split('&')]
+    return body
